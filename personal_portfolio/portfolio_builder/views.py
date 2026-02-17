1e2fc0e475895947ecd163ffd5503fc1f2673957
JmmonJeremy <suc19002@byui.edu>
2026-02-16 23:31:56 -0800

    Created 2 starting views and templates

--- personal_portfolio/portfolio_builder/views.py
+++ personal_portfolio/portfolio_builder/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django import forms
 
-# Create your views here.
+# LEARN Create a simple form class
+class PortfolioForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea)
+
+# LEARN Home page view
+def home(request):   
+    return render(request, "portfolio_builder/home.html")
+
+# LEARN Form submission view
+def contact(request):
+    if request.method == "POST":
+        form = PortfolioForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            message = form.cleaned_data["message"]
+            return render(
+                request,
+                "portfolio_builder/home.html",
+                {
+                    "success": True,
+                    "name": name,
+                    "message": message,
+                },
+            )
+    else:
+        form = PortfolioForm()
+    return render(request, "portfolio_builder/form.html", {"form": form})
