10cbcf625815851127c4e39b952a34e09a0eb9e0
JmmonJeremy <suc19002@byui.edu>
2026-02-18 23:32:29 -0800

    Added static CSS files and an image

--- personal_portfolio/portfolio_builder/urls.py
+++ personal_portfolio/portfolio_builder/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    # LEARN
+    path('', views.home, name='home'),   
     path("contact/", views.contact, name="contact"),
 ]
--- personal_portfolio/portfolio_builder/views.py
+++ personal_portfolio/portfolio_builder/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django import forms
 
-# LEARN Create a simple form class
+# LEARN Create a simple form class that inherits Django's base class for creating forms
 class PortfolioForm(forms.Form):
     name = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
 
-# LEARN Home page view
+# LEARN Home page view passing in request to handle web request (GET, POST, user data, etc.)
 def home(request):   
     return render(request, "portfolio_builder/home.html")
 
